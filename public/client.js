048130d7b22d755de4ed6c31861bb0007a8c0070
enomorino <enesherifi5@gmail.com>
2026-03-03 16:28:08 +0100

    Basic login functionality

--- public/client.js
+++ public/client.js
@@ -1,4 +1,7 @@
+let loggedIn = false;
+
 console.log("client script loaded!!");
+
 
 fetch('/api/stores')
     .then(response => response.json())
@@ -32,11 +35,35 @@ fetch('/api/stores')
     
     }) 
 
-    fetch("/api/login", {
+
+
+
+/// prettier break
+
+const loginForm = document.getElementById("loginForm");
+const submit = document.getElementById("submit");
+
+loginForm.addEventListener("submit", async (e) =>{
+    e.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    const res = await fetch("/login", {
         method: "POST",
         headers: {"Content-Type": "application/json"},
         body: JSON.stringify({ username, password})
-    })
-    .then(res => res.json())
-    .then(result => {return res.json({message: "sucessfully logged in"});
     });
+    const data = await res.json();
+
+    if (!res.ok){
+        loggedIn = false;
+        return;
+    }
+
+    loggedIn = true;
+    
+
+
+})
+
--- server.js
+++ server.js
@@ -11,17 +11,17 @@ const storeData = require("./stores.json");
 app.use('/', express.static('public'));
 app.use(express.json());
 
-app.get('/api/login', (req, res) => { 
+app.get('/login', (req, res) => { 
     res.send(`<!DOCTYPE html>
   <html>
     <body>
       <h1>Login</h1>
-      <form method="POST" action="/login">
+      <form method="POST" action="/login" id="loginForm">
         <label for="username">Username:</label>
         <input type="text" name="username" id="username" required /><br/><br/>
         <label for="password">Password:</label>
         <input type="password" name="password" id="password" required /><br/><br/>
-        <button type="submit">Login</button>
+        <button type="submit" id="submit">Login</button>
       </form>
       <hr>
       <h1>Store List</h1>
@@ -36,23 +36,23 @@ app.get('/api/stores', (req, res) =>{
 console.log("data loaded:", storeData);
 
 //login form
-// let isLoggedIn = false;
-// app.post ("/api/login", (req, res) =>{
-//   const {username, password }= req.body;
+ let isLoggedIn = false;
+ app.post ("/login", (req, res) =>{
+   const {username, password }= req.body;
 
-//   if(username === admin && password === adminPass){
-//     // isLoggedIn = true;
-//     return res.json({message: "sucessfully logged in"});
-//   }
-//   res.status(401).json({message: "errooooor logging in"});
-// });
+   if(username === admin && password === adminPass){
+     isLoggedIn = true;
+    return res.json({message: "sucessfully logged in"});
+  }
+  res.status(401).json({message: "errooooor logging in"});
+});
 
-// app.post("/api/logout", (req, res) =>
-// {
-//   isLoggedIn = false; 
+app.post("/api/logout", (req, res) =>
+{
+  isLoggedIn = false; 
 
-//   res.json({message: "succesfully logged out"});
-// } )
+  res.json({message: "succesfully logged out"});
+} )
 
 app.listen(PORT, ()=>{
     console.log(`Server running at http://localhost:${PORT}`);
