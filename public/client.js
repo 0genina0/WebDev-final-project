14af684de86054eddc3a0127d21e8d37888c760b
karastojanova <148975384+karastojanova@users.noreply.github.com>
2026-03-04 12:11:17 +0100

    FIXED Sorting code

--- public/client.js
+++ public/client.js
@@ -1,8 +1,10 @@
 let loggedIn = false;
-let body = document.querySelector("body");
+const body = document.querySelector("body");
 console.log("client script loaded!!");
 let selectOption = document.getElementById("filter");
 
+let isSorted = false;
+let listDrawn = false;
 
 let loginPageButton = document.createElement("button");
 loginPageButton.textContent = "login";
@@ -12,93 +14,87 @@ loginPageButton.addEventListener("click", ()=> {
     window.location.href = "/login";
 })
 
-let isSorted = false;
-let listDrawn = false;
-
+// Function in order to clear the UlList
 function reDrawList() {
   const venueList = document.getElementById("venueUl");
   venueList.innerHTML = "";
   listDrawn = false;
 }
 
+// Function to draw the full list
 function drawFullList(data) {
-    listDrawn = true;
     reDrawList();
+    listDrawn = true;
     let venueList = document.getElementById('venueUl');
 
-        data.forEach((store) => { 
+    data.forEach((store) => { 
 
-            let newStore = document.createElement("li");
+        let newStore = document.createElement("li");
 
-            newStore.textContent = store.name;
+        newStore.textContent = store.name;
 
-            let newStoreLink = document.createElement("a");
+        let newStoreLink = document.createElement("a");
 
-            newStoreLink.href = store.url;
-            newStoreLink.textContent = "Visit Website";
+        newStoreLink.href = store.url;
+        newStoreLink.textContent = "Visit Website";
 
-            let newStoreDistrict = document.createElement("span");
+        let newStoreDistrict = document.createElement("span");
 
-            newStoreDistrict.textContent = store.district;
+        newStoreDistrict.textContent = store.district;
 
-            venueList.appendChild(newStore);
+        venueList.appendChild(newStore);
 
-            venueList.appendChild(newStoreDistrict);
+        venueList.appendChild(newStoreDistrict);
 
-            venueList.appendChild(newStoreLink);
-        })}
+        venueList.appendChild(newStoreLink);
+})}
 
-function drawSortedList() {
-    listDrawn = true;
+// FUnction to draw the sorted list by district
+function drawSortedList(data) {
+    //listDrawn = true;
     reDrawList();
-  let venueList = document.getElementById('venueUl');
+    let venueList = document.getElementById('venueUl');
     const selectedDistrict = selectOption.value;
 
-  const filtered = data.filter(store => store.district === selectedDistrict);
+    const filtered = data.filter(store => store.district === selectedDistrict);
 
-                filtered.forEach((store) => {
-                    let sortedStore = document.createElement("li");
+        filtered.forEach((store) => {
+            let sortedStore = document.createElement("li");
         
-                    sortedStore.textContent = store.name;
+            sortedStore.textContent = store.name;
         
-                    let sortedStoreLink = document.createElement("a");
+            let sortedStoreLink = document.createElement("a");
         
-                    sortedStoreLink.href = store.url;
-                    sortedStoreLink.textContent = "Visit Website";
+            sortedStoreLink.href = store.url;
+            sortedStoreLink.textContent = "Visit Website";
         
-                    let sortedStoreDistrict = document.createElement("span");
+            let sortedStoreDistrict = document.createElement("span");
         
-                    sortedStoreDistrict.textContent = store.district;
+            sortedStoreDistrict.textContent = store.district;
         
-                    venueList.appendChild(sortedStore);
+            venueList.appendChild(sortedStore);
         
-                    venueList.appendChild(sortedStoreDistrict);
+            venueList.appendChild(sortedStoreDistrict);
         
-                    venueList.appendChild(sortedStoreLink);  })}
+            venueList.appendChild(sortedStoreLink);  
+})}
     
-            
-            
-
-
+// Fetching and displaying the list through a boolean from the JSON file
 fetch('/api/stores')
     .then(response => response.json())
     .then(data => {
     drawFullList(data);
         selectOption.addEventListener('change', ()=>{
-        })
-        if (selectOption.value === "location"){
+            reDrawList();
+        if (selectOption.value === "All"){
         drawFullList(data);
       } else {
         drawSortedList(data);
       }
-    });
-    
+    })
+});
 
-
-
-
-/// prettier break
-
+// LOG IN Form
 const loginForm = document.getElementById("loginForm");
 const submit = document.getElementById("submit");
 
@@ -120,8 +116,4 @@ loginForm.addEventListener("submit", async (e) =>{
     }
 
     loggedIn = true;
-    
-
-
 })
-
